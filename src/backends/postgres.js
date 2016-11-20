3f67343fcf866821ad1954b8ae8d9469c0dae05d
Matt Murphy <m.ryanmurphy@gmail.com>
2016-11-20 02:35:38 -0500

    Insert player data, get ready for box scores

--- src/backends/postgres.js
+++ src/backends/postgres.js
@@ -43,6 +43,8 @@ PostgresBackend.prototype.flush = function (type, data) {
 		
 		case 'player':
 			this.AddOrUpdatePlayer(data);
+			// this.GetPlayer(data);
+			//  - > this.AddOrUpdatePlayerStatistics(data);
 			break;
 	}
 };
@@ -50,6 +52,18 @@ PostgresBackend.prototype.flush = function (type, data) {
 PostgresBackend.prototype.CacheTeam = function(team) {
 	if (!this.cache.teams[team.id]) {
 		this.cache.teams[team.id] = team;
+	}
+};
+
+PostgresBackend.prototype.CacheGame = function(game) {
+	if (!this.cache.games[game.id]) {
+		this.cache.games[game.id] = game;
+	}
+};
+
+PostgresBackend.prototype.CachePlayer = function(player) {
+	if (!this.cache.players[player.id]) {
+		this.cache.players[player.id] = player;
 	}
 };
 
@@ -86,6 +100,15 @@ RETURNING ID
 
 // TODO: This gotta be refactored to promise/use cache/async+await or something.
 PostgresBackend.prototype.GetTeam = function(data, callback) {
+	var find = this.cache.teams.find(function(element) {
+		return element.city && element.city === data.city &&
+		       element.name && element.name === data.name;
+	});
+	if (find) {
+		callback(find);
+		return;
+	}
+	
 	const sql = `
 SELECT ID
 FROM League_Teams
@@ -119,16 +142,22 @@ RETURNING ID
 `;
 
 	var self = this,
-		homeID = data.side === 'home' ? data.teamID : null,
-		awayID = data.side === 'away' ? data.teamID : null,
-		homeScore = data.side === 'home' ? data.score : null,
-		awayScore = data.side === 'away' ? data.score : null;
+		cache = {
+			gameID: data.gameID,
+			homeID: data.side === 'home' ? data.teamID : null,
+			awayID: data.side === 'away' ? data.teamID : null,
+			homeScore: data.side === 'home' ? data.score : null,
+			awayScore: data.side === 'away' ? data.score : null
+		};
 
-	pool.query(sql, [data.gameID, homeID, awayID, homeScore, awayScore], function (err, result) {
+	pool.query(sql, [cache.gameID, cache.homeID, cache.awayID, cache.homeScore, cache.awayScore], function (err, result) {
 		if (err) {
 			self.logger.log(err, 'ERR');
 		} else if (result.rows.length) {
 			self.logger.log(`Saved score for ID ${result.rows[0].id} SourceID ${data.gameID}`, 'DEBUG');
+
+			cache.id = result.rows[0].id;
+			self.CacheGame(cache);
 		} else {
 			self.logger.log('No action for AddOrUpdateScore','DEBUG');
 		}
@@ -163,11 +192,142 @@ RETURNING ID
 };
 
 PostgresBackend.prototype.AddOrUpdatePlayer = function(data) {
-	this.AddOrUpdatePlayerStatistics(data);
+	const sql = `
+INSERT INTO League_Players (SourceID, Name, ActiveTeamID, IsGuard, IsForward, IsCenter)
+VALUES ($1, $2, $3, $4, $5, $6)
+ON CONFLICT (SourceID) DO UPDATE SET
+	Name = EXCLUDED.Name
+	, ActiveTeamID = EXCLUDED.ActiveTeamID
+	, IsGuard = EXCLUDED.IsGuard
+	, IsForward = EXCLUDED.IsForward
+	, IsCenter = EXCLUDED.IsCenter
+RETURNING ID
+`;
+	
+	var self = this,
+		isGuard = /g/i.test(data.position),
+		isForward = /f/i.test(data.position),
+		isCenter = /c/i.test(data.position),
+		teamID = -1;
+	
+	// Lookup team by gameID + side (home/away)
+	pool.query(sql, [data.playerID, data.name, teamID, isGuard, isForward, isCenter], function(err, result) {
+		if (err) {
+			self.logger.log(err, 'ERR');
+		} else if (result.rows.length) {
+			self.logger.log(`Saved player for ID ${result.rows[0].id} SourceID ${data.gameID} [${data.name}]`, 'DEBUG');
+
+			// Make sure teamID gets cached too.
+			data.id = result.rows[0];
+			self.CachePlayer(data);
+		} else {
+			self.logger.log('No action for AddOrUpdatePlayer', 'DEBUG');
+		}
+	});
 };
 
+// must have gameID and playerID to INSERT
+// should have teamID 
 PostgresBackend.prototype.AddOrUpdatePlayerStatistics = function(data) {
+	var self = this;
+
+	function onInsert(err, result) {
+		if (err) {
+			self.logger.log(err, 'ERR');
+		} else if (result.rows.length) {
+			self.logger.log(`Saved box for ID ${result.rows[0].id}`, 'DEBUG');
+		} else {
+			self.logger.log('No action for AddOrUpdatePlayerStatistics', 'DEBUG');
+		}
+	};
 	
+	const sql = `
+INSERT INTO League_BoxScores (
+	GameID
+	, TeamID
+	, PlayerID
+	, IsDNP
+	, DNPStatus
+	, Minutes
+	, Points
+	, PlusMinus
+	, OffensiveRebounds
+	, DefensiveRebounds
+	, Assists
+	, Steals
+	, Blocks
+	, Turnovers
+	, Fouls
+	, FreeThrowsMade
+	, FreeThrowsAttempted
+	, TwoPointsMade
+	, TwoPointsAttempted
+	, ThreePointsMade
+	, ThreePointsAttempted
+) VALUES (
+	$1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,
+	$12, $13, $14, $15, $16, $17, $18, $19, $20, $21
+)
+ON CONFLICT (GameID, PlayerID) DO UPDATE SET
+	TeamID = EXCLUDED.TeamID
+	, IsDNP = EXCLUDED.IsDNP
+	, DNPStatus = EXCLUDED.DNPStatus
+	, Minutes = EXCLUDED.Minutes
+	, Points = EXCLUDED.Points
+	, PlusMinus = EXCLUDED.PlusMinus
+	, OffensiveRebounds = EXCLUDED.OffensiveRebounds
+	, DefensiveRebounds = EXCLUDED.DefensiveRebounds
+	, Assists = EXCLUDED.Assists
+	, Steals = EXCLUDED.Steals
+	, Blocks = EXCLUDED.Blocks
+	, Turnovers = EXCLUDED.Turnovers
+	, Fouls = EXCLUDED.Fouls
+	, FreeThrowsMade = EXCLUDED.FreeThrowsMade
+	, FreeThrowsAttempted = EXCLUDED.FreeThrowsAttempted
+	, TwoPointsMade = EXCLUDED.TwoPointsMade
+	, TwoPointsAttempted = EXCLUDED.TwoPointsAttempted
+	, ThreePointsMade = EXCLUDED.ThreePointsMade
+	, ThreePointsAttempted = EXCLUDED.ThreePointsAttempted
+RETURNING ID
+`, dnpSQL = `
+INSERT INTO League_BoxScores (GameID, TeamID, PlayerID, IsDNP, DNPStatus)
+VALUES ($1, $2, $3, $4, $5)
+ON CONFLICT (GameID, PlayerID) DO UPSET SET
+	TeamID = EXCLUDED.TeamID
+	, IsDNP = EXCLUDED.IsDNP
+	, DNPStatus = EXCLUDED.DNPStatus
+RETURNING ID
+`;
+
+	var teamID = -1; // Need to lookup teamID~!
+	
+	if (data.dnp) {
+		pool.query(dnpSQL, [data.gameID, teamID, data.id, true, data.dnp_reason], onInsert);
+	} else {
+		pool.query(sql, [
+			data.gameID, 
+			teamID, 
+			data.id, 
+			false, 
+			null,
+			data.minutes,
+			data.points,
+			data.plusMinus,
+			data.offensiveRebounds,
+			data.defensiveRebounds,
+			data.assists,
+			data.steals,
+			data.blocks,
+			data.turnovers,
+			data.fouls,
+			data.freeThrowsMade,
+			data.freeThrowsAttempted,
+			data.twoPointsMade,
+			data.twoPointsAttempted,
+			data.threePointsMade,
+			data.threePointsAttempted
+		], onInsert);
+	}
 };
 
 exports.init = function (config, emitter, logger) {
